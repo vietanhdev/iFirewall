7a751b906939f93a7e572ec924f476fdee2a98e3
Viet-Anh, Nguyen <vietanh@vietanhdev.com>
2019-11-13 12:11:31 +0700

    Remove path filter

--- iWAF/app.py
+++ iWAF/app.py
@@ -36,37 +36,12 @@ def proxy(url=""):
     out.status_code = r.status_code
     return out
 
-
 def make_request(url, method, headers={}, data=None):
     url = '%s' % url
-
-    # Pass original Referer for subsequent resource requests
-    referer = request.headers.get('referer')
-    # if referer:
-        # proxy_ref = proxied_request_info(referer)
-        # headers.update({ "referer" : "http://%s/%s" % (proxy_ref[0], proxy_ref[1])})
 
     # Fetch the URL, and stream it back
     LOG.debug("Sending %s %s with headers: %s and data %s", method, url, headers, data)
     return requests.request(method, url, params=request.args, stream=True, headers=headers, allow_redirects=False, data=data)
-
-def proxied_request_info(proxy_url):
-    """Returns information about the target (proxied) URL given a URL sent to
-    the proxy itself. For example, if given:
-        http://localhost:5000/p/google.com/search?q=foo
-    then the result is:
-        ("google.com", "search?q=foo")"""
-    parts = urlparse(proxy_url)
-    if not parts.path:
-        return None
-    elif not parts.path.startswith('/p/'):
-        return None
-    matches = re.match('^/p/([^/]+)/?(.*)', parts.path)
-    proxied_host = matches.group(1)
-    proxied_path = matches.group(2) or '/'
-    proxied_tail = urlunparse(parts._replace(scheme="", netloc="", path=proxied_path))
-    LOG.debug("Referred by proxy host, uri: %s, %s", proxied_host, proxied_tail)
-    return [proxied_host, proxied_tail]
 
 
 if __name__ == '__main__':
