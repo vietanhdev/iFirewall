36d316f1e806fb4368578f71b5ed4bff925e99f9
Viet-Anh, Nguyen <vietanh@vietanhdev.com>
2019-11-13 14:28:37 +0700

    Use multiple origin servers

--- iWAF/app.py
+++ iWAF/app.py
@@ -1,10 +1,11 @@
-from config import RATE_LIMITER_CONF
+from config import RATE_LIMITER_CONF, SERVERS
 from flask import Flask, render_template, jsonify, request, redirect, Response, request, abort
 import json
 import re
 from urllib.parse import urlparse, urlunparse
 import requests
 import logging
+import random
 
 app = Flask(__name__)
 
@@ -13,17 +14,13 @@ from RateLimiter import RateLimiter
 # Initialize Firewall
 limiter = RateLimiter(RATE_LIMITER_CONF)
 
-ORIGIN_SERVER = "http://web_server_1:80/"
-
 LOG = logging.getLogger("app.py")
 
 
 @app.route('/', methods=["GET", "POST", "PUT", "DELETE"])
 @app.route('/<path:url>', methods=["GET", "POST", "PUT", "DELETE"])
-@limiter.rate_limit(limit=10, interval=60)
+@limiter.rate_limit(limit=RATE_LIMITER_CONF["global_limit"]["limit"], interval=RATE_LIMITER_CONF["global_limit"]["interval"])
 def proxy(url=""):
-
-    url = ORIGIN_SERVER + url
 
     LOG.debug("%s %s with headers: %s", request.method, url, request.headers)
     r = make_request(url, request.method, dict(request.headers), request.form)
@@ -37,7 +34,8 @@ def proxy(url=""):
     return out
 
 def make_request(url, method, headers={}, data=None):
-    url = '%s' % url
+
+    url = SERVERS[random.randint(0, len(SERVERS)-1)]["address"] + url
 
     # Fetch the URL, and stream it back
     LOG.debug("Sending %s %s with headers: %s and data %s", method, url, headers, data)
--- iWAF/config.py
+++ iWAF/config.py
@@ -4,5 +4,29 @@ RATE_LIMITER_CONF = {
         "host": "redis",
         "port": 6379,
         "db": 0
+    },
+
+    "global_limit": {
+        "limit": 120,
+        "interval": 60
     }
 }
+
+SERVERS = [
+    {
+        "address": "http://web_server_1:80/"
+    },
+    {
+        "address": "http://web_server_2:80/"
+    },
+    {
+        "address": "http://web_server_3:80/"
+    },
+    {
+        "address": "http://web_server_4:80/"
+    },
+    {
+        "address": "http://web_server_5:80/"
+    },
+]
+
