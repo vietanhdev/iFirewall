58b0037e51e288210210669d178301cf486e4b33
Viet-Anh, Nguyen <vietanh@vietanhdev.com>
2019-11-16 09:43:58 +0700

    Save params to server

--- iWAF/Config.py
+++ iWAF/Config.py
@@ -45,5 +45,49 @@ class Config:
         self.data_lock.release()
 
     def update_params(self, params):
+
+        for key in params:
+            
+            # Int params
+            if key in ["rate_limit_interval",
+                "rate_limit_warning_thresh",
+                "rate_limit_block_thresh",
+                "rate_limit_block_time",
+                "rate_limit_ddos_thresh",
+                "rate_limit_ddos_interval",
+                "rate_limit_whitelist_expiration_time"]:
+            
+                try:
+                    value = int(params[key])
+                    if value <= 0:
+                        return "WTF"
+                        return "Wrong value for %s!" % key
+                    self.set_param(key, value)
+                except:
+                    return "Wrong value for %s!" % key
+
+            elif key == "rate_limit_under_attack":
+                try:
+                    value = bool(params[key])
+                    self.set_param(key, value)
+                except:
+                    return "Wrong value for %s!" % key
+
+            elif key == "servers":
+                # try:
+                servers = []
+                for i in range(len(params["servers"])):
+                    servers.append({
+                        "id": i+1,
+                        "address": params["servers"][i],
+                        "server_status_url": params["servers"][i] + "server_status",
+                        "online": True
+                    })
+                self.set_param(key, servers)
+                # except:
+                #     return "Wrong value for %s!" % key
+
+
+
         return "Updated successfully!"
     
