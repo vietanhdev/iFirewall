c1416643a700c1ed847cf613cd58cf35f39a4b82
Viet-Anh, Nguyen <vietanh@vietanhdev.com>
2019-11-16 12:42:47 +0700

    Update default config

--- iWAF/default_config.py
+++ iWAF/default_config.py
@@ -10,9 +10,9 @@ DEFAULT_CONFIG = {
     "rate_limit_warning_thresh": 20,
     "rate_limit_block_thresh": 30,
     "rate_limit_block_time": 300,
-    "rate_limit_ddos_thresh": 5,
+    "rate_limit_ddos_thresh": 60,
     "rate_limit_ddos_interval": 60,
-    "rate_limit_ddos_blocking_time": 60,
+    "rate_limit_ddos_blocking_time": 300,
     "rate_limit_whitelist_expiration_time": 10*60,
     "rate_limit_under_attack": False,
 
